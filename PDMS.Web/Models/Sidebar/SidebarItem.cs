ae3a7da5d966bf5baea4ca87621b36bc67ce648c
QuanLe <zedovblack@gmail.com>
2024-02-04 06:47:37 +0700

    add basic layout and sidebar structure

---
+++ PDMS.Web/Models/Sidebar/SidebarItem.cs
@@ -0,0 +1,13 @@
+﻿namespace PDMS.Web.Models.Sidebar;
+
+public class SidebarItem : List<SidebarItem> {
+    public string? Icon { get; set; }
+    public string Name { get; set; }
+    public string Href { get; set; }
+
+    public SidebarItem(string name, string href = "", string? icon = null) {
+        Icon = icon;
+        Name = name;
+        Href = href;
+    }
+}
---
+++ PDMS.Web/Models/Sidebar/SidebarSection.cs
@@ -0,0 +1,9 @@
+﻿namespace PDMS.Web.Models.Sidebar;
+
+public class SidebarSection : List<SidebarItem> {
+    public string? Title { get; set; }
+
+    public SidebarSection(string? title = null) {
+        Title = title;
+    }
+}
