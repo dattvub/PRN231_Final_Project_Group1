ecc0d62723797abdca84bbf05ece19ecb95b2a10
Phạm Trọng Thành <cr10tnvl@gmail.com>
2024-02-06 21:10:15 +0700

    tét

--- PDMS.Web/Controllers/SupplierController.cs
+++ PDMS.Web/Controllers/SupplierController.cs
@@ -2,4 +2,26 @@
 
 namespace PDMS.Web.Controllers;
 
-public class SupplierController : Controller { }
+public class SupplierController : Controller {
+    [Route("Category/[controller]")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [Route("Category/[controller]/Add")]
+    public IActionResult Add()
+    {
+        return View();
+    }
+    [Route("Category/[controller]/{id}")]
+    public IActionResult Delete()
+    {
+        return View();
+    }
+
+    [Route("Category/[controller]/{id}/Update")]
+    public IActionResult Update()
+    {
+        return View();
+    }
+}
