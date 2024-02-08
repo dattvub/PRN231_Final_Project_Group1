da7861ef0d004aefb9534adbb07575cc49565cb1
QuanLe <zedovblack@gmail.com>
2024-02-08 18:46:37 +0700

    add update, delete brand api

--- PDMS/Controllers/BrandController.cs
+++ PDMS/Controllers/BrandController.cs
@@ -69,19 +69,57 @@ public class BrandController : ControllerBase {
         } catch (DbUpdateException) {
             if (!await CodeExisted(brand.BrandCode)) throw;
 
-            var error = new ValidationError() {
-                StatusCode = StatusCodes.Status400BadRequest,
-                ReasonPhrase = ReasonPhrases.GetReasonPhrase(StatusCodes.Status400BadRequest),
-                Errors = new List<string>() {
-                    "Dupplicate brand code"
-                },
-                Timestamp = DateTime.Now
-            };
+            return ValidationError.BadRequest400("Dupplicate brand code");
+        } catch (Exception e) {
+            return ValidationError.InternalServerError500(e.Message);
+        }
+    }
+
+    [EnableCors("allowAll")]
+    [HttpPut("{id:int:min(1)}")]
+    public async Task<ActionResult<BrandDto>> UpdateBrand(
+        [FromRoute] int id,
+        [FromBody] CreateBrandDto createBrandDto
+    ) {
+        var brand = await _context.Brands.FindAsync(id);
+        if (brand == null) {
+            return ValidationError.BadRequest400($"Brand with id {id} is not exist");
+        }
+
+        var newInfoBrand = _mapper.Map<Brand>(createBrandDto);
+        brand.BrandCode = newInfoBrand.BrandCode;
+        brand.BrandName = newInfoBrand.BrandName;
+        try {
+            var result = await _context.SaveChangesAsync();
+            if (result == 0) {
+                return ValidationError.BadRequest400("Error occur while update brand");
+            }
+        } catch (DbUpdateException) {
+            if (!await CodeExisted(brand.BrandCode)) throw;
 
             return ValidationError.BadRequest400("Dupplicate brand code");
         } catch (Exception e) {
             return ValidationError.InternalServerError500(e.Message);
         }
+
+        return Ok(_mapper.Map<BrandDto>(brand));
+    }
+
+    [EnableCors("allowAll")]
+    [HttpDelete("{id:int:min(1)}")]
+    public async Task<ActionResult<BrandDto>> DeleteBrand([FromRoute] int id) {
+        var brand = await _context.Brands.FindAsync(id);
+        if (brand == null) {
+            return ValidationError.BadRequest400($"Brand with id {id} is not exist");
+        }
+
+        brand.Status = false;
+        var result = await _context.SaveChangesAsync();
+        if (result == 0) {
+            return ValidationError.InternalServerError500("Error occur while delete brand");
+        }
+
+        return Ok(_mapper.Map<BrandDto>(brand));
     }
 
     private async Task<bool> CodeExisted(string code) {
