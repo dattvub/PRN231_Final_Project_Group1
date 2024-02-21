7622e8732256a549e306c267d9851ab29052618b
QuanLe <zedovblack@gmail.com>
2024-02-21 11:58:29 +0700

    add employee create ui

--- PDMS.Web/Controllers/EmployeeController.cs
+++ PDMS.Web/Controllers/EmployeeController.cs
@@ -7,4 +7,9 @@ public class EmployeeController : Controller {
     public IActionResult Index() {
         return View();
     }
+
+    [Route("Create")]
+    public IActionResult Create() {
+        return View();
+    }
 }
---
+++ PDMS.Web/wwwroot/js/employee/employee-create.js
@@ -0,0 +1,23 @@
+﻿const mainForm = $('#mainForm')
+const avatarInput = $('#avatar')
+
+mainForm.on('submit', e => {
+    e.preventDefault()
+    e.stopPropagation()
+
+    if (mainForm.find(':invalid').length > 0) {
+        mainForm.addClass('was-validated')
+        return
+    }
+
+    console.log(123)
+})
+
+avatarInput.on('input', e => {
+    const file = e.delegateTarget.files[0];
+    if (file) {
+        $('#preview-avatar').removeClass('d-none')[0].src = URL.createObjectURL(file)
+    } else {
+        $('#preview-avatar').addClass('d-none')[0].src = ''
+    }
+})
