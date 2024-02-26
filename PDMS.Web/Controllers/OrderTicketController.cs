7ce21469be75c30593ab04faca30000644d412f3
DESKTOP-0LFAVEB\ADMIN <phitiendat1104@gmail.com>
2024-02-26 15:54:12 +0700

    major

--- PDMS.Web/Controllers/OrderTicketController.cs
+++ PDMS.Web/Controllers/OrderTicketController.cs
@@ -1,5 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace PDMS.Web.Controllers;
+namespace PDMS.Web.Controllers; 
+public class OrderTicketController : Controller {
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-public class OrderTicketController : Controller { }
+  
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    public IActionResult Detail()
+    {
+        return View();
+    }
+    public IActionResult Update()
+    {
+        return View();
+    }
+}
