3773d7e6b6a6cad1a2ca21693e40beaf1ca3b41a
Nam Bui Hoai <hoainamnd1@gmail.com>
2024-02-28 14:03:51 +0700

    Update customer type

--- PDMS.Web/Controllers/CustomerTypeController.cs
+++ PDMS.Web/Controllers/CustomerTypeController.cs
@@ -1,55 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using PDMS.Domain.Entities;
-using PDMS.Infrastructure.Persistence;
 using PDMS.Web.Models.Sidebar;
 
 namespace PDMS.Web.Controllers;
 
 [Route("Category/CustomerType")]
 public class CustomerTypeController : Controller {
-    [BindProperty(SupportsGet = true)]
-    public IList<CustomerType> CustomerTypes { get; set; } = default!;
-
-    [BindProperty(SupportsGet = true)]
-    public int CustomerTypeId { get; set; } = default!;
-
     public CustomerTypeController()
     {
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<CustomerType>>> Index() {
-        addSidebar();
-        CustomerTypes = new List<CustomerType>() {
-            new CustomerType{ CustomerTypeId = 1,CustomerTypeCode = "zero",CustomerTypeName="segg", Status= true},
-            new CustomerType { CustomerTypeId = 2, CustomerTypeCode = "one", CustomerTypeName = "segg", Status = false },
-            new CustomerType { CustomerTypeId = 3, CustomerTypeCode = "two", CustomerTypeName = "segg", Status = true },
-            new CustomerType { CustomerTypeId = 4, CustomerTypeCode = "three", CustomerTypeName = "segg", Status = false },
-            new CustomerType { CustomerTypeId = 5, CustomerTypeCode = "four", CustomerTypeName = "segg", Status = true },
-            new CustomerType { CustomerTypeId = 6, CustomerTypeCode = "five", CustomerTypeName = "segg", Status = false },
-            new CustomerType { CustomerTypeId = 7, CustomerTypeCode = "six", CustomerTypeName = "segg", Status = true },
-            new CustomerType { CustomerTypeId = 8, CustomerTypeCode = "seven", CustomerTypeName = "segg", Status = false },
-            new CustomerType { CustomerTypeId = 9, CustomerTypeCode = "eight", CustomerTypeName = "segg", Status = true },
-            new CustomerType { CustomerTypeId = 10, CustomerTypeCode = "nine", CustomerTypeName = "segg", Status = false }};
-        return View(CustomerTypes);
-    }
-
-    [Route("Add")]
-    [HttpGet]
-    public async Task<ActionResult<List<CustomerType>>> AddCustomerType()
-    {
-        addSidebar();
+    public IActionResult Index() {
         return View();
-    }
-
-    [Route("{id}/Update")]
-    [HttpGet]
-    public async Task<ActionResult<List<CustomerType>>> UpdateCustomerType([FromRoute] int id)
-    {
-        addSidebar();
-        CustomerTypeId = id;
-        return View(CustomerTypeId);
     }
 
     public void addSidebar()
