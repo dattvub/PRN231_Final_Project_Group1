7082a38b830e249f7e9138115a0a3240af02b333
QuanLe <zedovblack@gmail.com>
2024-02-28 11:30:40 +0700

    add create employee page

--- PDMS.Web/wwwroot/js/employee/employee-create.js
+++ PDMS.Web/wwwroot/js/employee/employee-create.js
@@ -1,5 +1,12 @@
 ﻿const mainForm = $('#mainForm')
 const avatarInput = $('#avatar')
+const [entranceDateInput, exitDateInput] = $('.datetimepicker').flatpickr({
+    disableMobile: true,
+    dateFormat: 'd-m-Y',
+    locale: 'vn'
+})
+const entranceDateSwitch = $('#entranceDateSwitch')
+const exitDateSwitch = $('#exitDateSwitch')
 
 mainForm.on('submit', e => {
     e.preventDefault()
@@ -24,4 +31,43 @@ avatarInput.on('input', e => {
 
 $('#btnReset').on('click', () => {
     mainForm[0].reset()
+    entranceDateSwitch[0].disabled = exitDateSwitch[0].checked
+    exitDateSwitch[0].disabled = !entranceDateSwitch[0].checked
+})
+
+$(entranceDateInput.element).hide()
+$(exitDateInput.element).hide()
+exitDateSwitch[0].disabled = !entranceDateSwitch[0].checked
+
+exitDateSwitch.on('change', () => {
+    if (!exitDateSwitch[0].checked) {
+        exitDateInput.clear()
+        exitDateInput.close()
+        if (exitDateInput.element.style.display !== 'none') {
+            $(exitDateInput.element).slideToggle(160)
+        }
+        entranceDateSwitch[0].disabled = false
+    } else {
+        if (exitDateInput.element.style.display === 'none') {
+            $(exitDateInput.element).slideToggle(160)
+        }
+        entranceDateSwitch[0].disabled = true
+    }
+})
+
+entranceDateSwitch.on('change', () => {
+    if (!entranceDateSwitch[0].checked) {
+        console.log(123)
+        entranceDateInput.clear()
+        entranceDateInput.close()
+        if (entranceDateInput.element.style.display !== 'none') {
+            $(entranceDateInput.element).slideToggle(160)
+        }
+        exitDateSwitch[0].disabled = true
+    } else {
+        if (entranceDateInput.element.style.display === 'none') {
+            $(entranceDateInput.element).slideToggle(160)
+        }
+        exitDateSwitch[0].disabled = false
+    }
 })
